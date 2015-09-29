8fb359e0d67b47ac21dda3bbd93c61c40595b91c
bmaturi <bmaturi@unico.com.au>
2015-09-29 00:39:00 -0400

    #noissue : initial project setup

---
+++ src/main/java/com/amdocs/driving/simulator/DrivingSimulator.java
@@ -0,0 +1,19 @@
+/**
+ *
+ */
+package com.amdocs.driving.simulator;
+
+/**
+ * @author Bharath Maturi
+ */
+public class DrivingSimulator {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
